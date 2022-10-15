0a342331a9d0663e9fcc87d77ae755c49843658f
10zinten <ten13zin@gmail.com>
2022-10-15 10:35:42 +0530

    Add page container component

---
+++ src/components/PageContainer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="q-ml-auto q-mr-auto" style="max-width: 1080px">
+    <slot></slot>
+  </div>
+</template>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,45 +1,48 @@
 <template>
-  <q-page padding style="max-width: 1080px; margin: auto">
-    <div>
-      <div class="text-h5">Worklog Generator</div>
-      <div class="q-mt-sm q-gutter-md">
-        <div class="row justify-between">
-          <div class="row q-gutter-md">
-            <q-input v-model="username" label="Github Username" filled />
-            <q-input v-model="date" label="Month" filled type="date" />
+  <q-page padding>
+    <PageContainer>
+      <div>
+        <div class="text-h5">Worklog Generator</div>
+        <div class="q-mt-sm q-gutter-md">
+          <div class="row justify-between">
+            <div class="row q-gutter-md">
+              <q-input v-model="username" label="Github Username" filled />
+              <q-input v-model="date" label="Month" filled type="date" />
+            </div>
+
+            <q-btn no-caps class="q-ml-lg" color="primary" label="Generate" />
           </div>
 
-          <q-btn no-caps class="q-ml-lg" color="primary" label="Generate" />
-        </div>
-
-        <div>
-          <q-select
-            filled
-            v-model="contribRepos"
-            multiple
-            :options="allRepos"
-            use-chips
-            stack-label
-            label="Contributing Repos"
-          />
+          <div>
+            <q-select
+              filled
+              v-model="contribRepos"
+              multiple
+              :options="allRepos"
+              use-chips
+              stack-label
+              label="Contributing Repos"
+            />
+          </div>
         </div>
       </div>
-    </div>
 
-    <q-separator dash class="q-mt-lg" />
+      <q-separator dash class="q-mt-lg" />
 
-    <div>
-      <div class="q-mt-md text-h5">Your Contributions</div>
       <div>
-        <ContributionList />
+        <div class="q-mt-md text-h5">Your Contributions</div>
+        <div>
+          <ContributionList />
+        </div>
       </div>
-    </div>
+    </PageContainer>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import ContributionList from '../components/ContributionList.vue';
+import ContributionList from 'components/ContributionList.vue';
+import PageContainer from 'components/PageContainer.vue';
 
 const username = ref('');
 
